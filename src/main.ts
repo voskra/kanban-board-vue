d965b146ce724cd1843f0a999dc0ada856db2440
voskra <n.voskresenskaya@dats.team>
2025-07-07 16:15:44 +0200

    add icons, fonts, scss, pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import './styles/base.scss'
+
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
